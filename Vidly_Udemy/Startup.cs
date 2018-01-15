33ef4b1302e9828ab9d549c1d8c181bf10b01cf9
gabee1987 <gabee1987@gmail.com>
2018-01-15 17:07:07 +0100

    Add project files.

---
+++ Vidly_Udemy/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(Vidly_Udemy.Startup))]
+namespace Vidly_Udemy
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
