f6c829d64d7ca617c6d3dba7189e9b703613f03c
gabee1987 <gabee1987@gmail.com>
2018-01-29 14:39:56 +0100

    Added test datas to db with nBuilder and Faker.NET

--- Vidly_Udemy/Controllers/CustomersController.cs
+++ Vidly_Udemy/Controllers/CustomersController.cs
@@ -8,17 +8,29 @@ namespace Vidly_Udemy.Controllers
 {
     public class CustomersController : Controller
     {
+        private ApplicationDbContext _context;
+
+        public CustomersController()
+        {
+            _context = new ApplicationDbContext();
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            _context.Dispose();
+        }
+
         // GET: Customers
         public ViewResult Index()
         {
-            var customers = GetCustomers();
+            var customers = _context.Customers.ToList();
 
             return View(customers);
         }
 
         public ActionResult Details(int id)
         {
-            var customer = GetCustomers().SingleOrDefault(c => c.Id == id);
+            var customer = _context.Customers.SingleOrDefault(c => c.Id == id);
 
             if (customer == null)
                 return HttpNotFound();
@@ -26,13 +38,14 @@ namespace Vidly_Udemy.Controllers
             return View(customer);
         }
 
-        private IEnumerable<Customer> GetCustomers()
-        {
-            return new List<Customer>
-            {
-                new Customer { Id = 1, Name = "Hanry Avery" },
-                new Customer { Id = 2, Name = "Francis Drake" }
-            };
-        }
+        // Get customers without database
+        //private IEnumerable<Customer> GetCustomers()
+        //{
+        //    return new List<Customer>
+        //    {
+        //        new Customer { Id = 1, Name = "Hanry Avery" },
+        //        new Customer { Id = 2, Name = "Francis Drake" }
+        //    };
+        //}
     }
 }
--- Vidly_Udemy/Migrations/Configuration.cs
+++ Vidly_Udemy/Migrations/Configuration.cs
@@ -1,3 +1,6 @@
+using System;
+using Vidly_Udemy.Models;
+using FizzWare.NBuilder;
 namespace Vidly_Udemy.Migrations
 {
     using System;
@@ -18,6 +21,19 @@ namespace Vidly_Udemy.Migrations
 
             //  You can use the DbSet<T>.AddOrUpdate() helper extension method 
             //  to avoid creating duplicate seed data.
+
+
+            // Create test datas with nBuilder and Faker.NET
+            Random rnd = new Random();
+            var customers = Builder<Customer>.CreateListOfSize(50)
+                .All()
+                    .With(c => c.Name = Faker.Name.FullName())
+                    .With(c => c.IsSubscribedToNewsletter = rnd.Next(100) <= 50 ? true : false)
+                    .With(c => c.MembershipTypeId = (byte)Faker.RandomNumber.Next(1, 4))
+                .Build();
+
+            context.Customers.AddOrUpdate(c => c.Id, customers.ToArray());
+            
         }
     }
 }
